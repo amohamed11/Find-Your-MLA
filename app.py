25ca3df20b71cfc3d6cac90a93e06045da10909d
Anas Mohamed <amohamed@ualberta.ca>
2018-05-29 11:40:37 -0600

    Added Form/Submission Handling
    Added the Form for for choosing one's riding.
    p.s this took way too long and i hate myself rn

--- app.py
+++ app.py
@@ -1,11 +1,45 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
 
 @app.route('/')
 def index():
-    
+    ridings = riding_list()
 
-    return render_template('index.html')
+    return render_template('index.html', ridings=ridings, chosen='')
+
+
+@app.route("/" , methods=['GET', 'POST'])
+def show_minister():
+    ridings = riding_list()
+    if request.method == 'GET':
+        chosen = ''
+        return render_template('index.html', ridings=ridings, chosen=chosen)
+    else:
+        chosen = request.form['chose']
+        print(chosen)
+        return render_template('index.html', ridings=ridings, chosen=chosen)
+
+
+    # c.execute('''SELECT * FROM ministers WHERE riding='%s' ''' % riding)
+    # rows = c.fetchall
+    # print(rows)
+
+def riding_list():
+    conn = sqlite3.connect("ministers.db")
+    c = conn.cursor()
+
+    c.execute('''SELECT riding FROM ministers ''')
+    rows = c.fetchall()
+    ridings = []
+    for row in rows:
+        ridings.append(row[0])
+    ridings.sort()
+
+    conn.close()
+
+    return ridings
+
+if __name__=='__main__':
+    app.run()
