9bc503d6e9f8118796848142210aef9e9502e77a
Anas Mohamed <amohamed@ualberta.ca>
2018-05-30 07:41:47 -0600

    Updated Functionality
    The website is now mostly functional.
    Added Information display when district is picked.
    Changed some variable names to more logical names.
    Also added some mobile responsiveness.

--- app.py
+++ app.py
@@ -5,41 +5,61 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    ridings = riding_list()
+    districts = district_list()
 
-    return render_template('index.html', ridings=ridings, chosen='')
+    return render_template('index.html', districts=districts, chosen='', info = '')
 
 
 @app.route("/" , methods=['GET', 'POST'])
 def show_minister():
-    ridings = riding_list()
+    districts = district_list()
     if request.method == 'GET':
-        chosen = ''
-        return render_template('index.html', ridings=ridings, chosen=chosen)
+        return render_template('index.html', districts=districts, chosen='', info = '')
     else:
         chosen = request.form['chose']
-        print(chosen)
-        return render_template('index.html', ridings=ridings, chosen=chosen)
+        info = get_info(chosen)
+        return render_template('index.html', districts=districts, chosen=chosen, info=info)
 
+def get_info(district):
+    conn = sqlite3.connect("ministers.db")
+    c = conn.cursor()
 
-    # c.execute('''SELECT * FROM ministers WHERE riding='%s' ''' % riding)
-    # rows = c.fetchall
-    # print(rows)
+    c.execute('''SELECT * FROM ministers WHERE riding='%s' ''' % district)
+    rows = c.fetchall()
 
-def riding_list():
+    member = rows[0][0]
+    district = rows[0][1]
+    party = rows[0][2]
+    email = rows[0][3]
+    phone = rows[0][4]
+    image_url = rows[0][5]
+
+    info = {
+        'name' : member,
+        'district' : district,
+        'party' : party,
+        'email' : email,
+        'phone' : phone.replace('.', '-'),
+        'image_url' : image_url
+    }
+
+    conn.close()
+    return info
+
+def district_list():
     conn = sqlite3.connect("ministers.db")
     c = conn.cursor()
 
     c.execute('''SELECT riding FROM ministers ''')
     rows = c.fetchall()
-    ridings = []
+    districts = []
     for row in rows:
-        ridings.append(row[0])
-    ridings.sort()
+        districts.append(row[0])
+    districts.sort()
 
     conn.close()
 
-    return ridings
+    return districts
 
 if __name__=='__main__':
     app.run()
